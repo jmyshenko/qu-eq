43830f36b855112c3043b6654528d63ee7a6156f
jmyshenko <73219477+jmyshenko@users.noreply.github.com>
2021-03-16 21:09:43 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const answer = document.getElementById('answer');
 
 
 function getParameters() {
-    const getParameterValue = item => Number.parseFloat(parameters.item(item).querySelector('input').value);
+    const getParameterValue = item => Number.parseFloat(parameters.item(item).value);
     
     const a = getParameterValue(0);
     const b = getParameterValue(1);
@@ -20,35 +20,50 @@ function getParameters() {
 
 
 calcDiscriminant.onclick = function() {
-    const [ paramA, paramB, paramC ] = getParameters();
+    const _parameters = getParameters();
+    if (!_parameters) return;
+    const [ paramA, paramB, paramC ] = _parameters; 
     const discriminant = (paramB * paramB) - ((paramA * paramC) * 4);
 
-    answer.innerHTML = `Дискриминант уравнения равен ${discriminant}.</br>`;
-
-    if (discriminant > 0) {
-        const root1 = -paramB + Math.sqrt(discriminant);
-        const root2 = -paramB - Math.sqrt(discriminant);
-
-        answer.innerHTML += `Поскольку дискриминант больше единицы, уравнения имеет два действительных корня:</br>`;
-        answer.innerHTML += `<div class="block" class="math">x<sub>1</sub> = ${root1.toFixed(2)}</div>`;
-        answer.innerHTML += `<div class="block" class="math">x<sub>2</sub> = ${root2.toFixed(2)}</div>`;
-    } else if (discriminant == 0) {
-        const root = -paramB / (paramA * 2);
+    let rootsLi = '';
+    if (discriminant < 0) rootsLi = `<li>Поскольку дискриминант меньше нуля, действительных корней нет</li>`;
+    else if (discriminant === 0) rootsLi = `<li>x = ${rounder(-paramB / (paramA * 2))}</li>`
+    else {
+        const rounder = number => Math.round(number * 100) / 100;
         
-        answer.innerHTML += `Поскольку дискриминант равен нулю, у уравнения есть единственный действительный корень:</br>`;
-        answer.innerHTML += `<div class="block" class="math">x = ${root.toFixed(2)}</div>`
-    } else {
-        answer.innerHTML += `Поскольку дискриминант меньше нуля, уравнение не имеет действительных корней.`;
+        const root1 = (-paramB + Math.sqrt(discriminant)) / (paramA * 2);
+        const root2 = (-paramB - Math.sqrt(discriminant)) / (paramA * 2);
+
+        rootsLi += `<li>x<sub>1</sub> = ${rounder(root1)}</li>`;
+        rootsLi += `<li>x<sub>2</sub> = ${rounder(root2)}</li>`
     }
+
+    answer.innerHTML = `
+        <h4>Дискриминант и корни</h4>
+        <div class="block">
+            <ul>
+                <li>D = ${discriminant}</li>
+                ${rootsLi}
+            </ul>
+        </div>
+    `
+    answer.innerHTML += `
+    <h4>Подсказки</h4>
+    <div class="block">
+        <ul>
+            <li>Формула дискриминанта: <i>b<sup>2</sup> - 4ac</i></li>
+            <li>Формула корней при D > 0: <i>(-b ± √D) / 2a</i></li>
+            <li>Формула корней при D = 0: <i>-b / 2a</i></li>
+            <li>При D < 0 <i>действительных корней нет.</i></li>
+        </ul>
+    </div>`
 }
 
 calcViet.onclick = function() {
-    let [ paramA, paramB, paramC ] = getParameters();
-    // parameters.item(0).querySelector('input').value = 1;
-    const rootsSum = -(paramA / paramB);
-    const rootsComp = paramC / paramA;
+    const _parameters = getParameters();
+    if (!_parameters) return;
+    const [ paramA, paramB, paramC ] = _parameters; 
+    const discriminant = (paramB * paramB) - ((paramA * paramC) * 4);
 
-    answer.innerHTML = `По теореме Виета, сумма корней уравнения равна отрицательному частному a на b, а произведение — частному c на a. Тогда:</br>`;
-    answer.innerHTML += `x<sub>1</sub> + x<sub>2</sub> = ${rootsSum.toFixed(2)}</br>`;
-    answer.innerHTML += `x<sub>1</sub>x<sub>2</sub> = ${rootsComp.toFixed(2)}`;
+    alert('Эта функциональность пока не реализована.');
 }
