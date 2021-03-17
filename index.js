cb3c1bd6968d97edfa0da060e61db067a8c38a22
jmyshenko <73219477+jmyshenko@users.noreply.github.com>
2021-03-17 08:03:27 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const calcDiscriminant = document.getElementById('calc-discriminant');
 const calcViet = document.getElementById('calc-viet');
 const answer = document.getElementById('answer');
 
+const rounder = number => Math.round(number * 100) / 100;
 
 function getParameters() {
     const getParameterValue = item => Number.parseFloat(parameters.item(item).value);
@@ -25,17 +26,16 @@ calcDiscriminant.onclick = function() {
     const [ paramA, paramB, paramC ] = _parameters; 
     const discriminant = (paramB * paramB) - ((paramA * paramC) * 4);
 
-    let rootsLi = '';
-    if (discriminant < 0) rootsLi = `<li>Поскольку дискриминант меньше нуля, действительных корней нет</li>`;
-    else if (discriminant === 0) rootsLi = `<li>x = ${rounder(-paramB / (paramA * 2))}</li>`
+    let roots = '';
+    if (discriminant < 0) roots = `<li>Поскольку дискриминант меньше нуля, действительных корней нет</li>`;
+    else if (discriminant === 0) roots = `<li>x = ${rounder(-paramB / (paramA * 2))}</li>`
     else {
-        const rounder = number => Math.round(number * 100) / 100;
         
         const root1 = (-paramB + Math.sqrt(discriminant)) / (paramA * 2);
         const root2 = (-paramB - Math.sqrt(discriminant)) / (paramA * 2);
 
-        rootsLi += `<li>x<sub>1</sub> = ${rounder(root1)}</li>`;
-        rootsLi += `<li>x<sub>2</sub> = ${rounder(root2)}</li>`
+        roots += `<li>x<sub>1</sub> = ${rounder(root1)}</li>`;
+        roots += `<li>x<sub>2</sub> = ${rounder(root2)}</li>`
     }
 
     answer.innerHTML = `
@@ -43,7 +43,7 @@ calcDiscriminant.onclick = function() {
         <div class="block">
             <ul>
                 <li>D = ${discriminant}</li>
-                ${rootsLi}
+                ${roots}
             </ul>
         </div>
     `
@@ -65,5 +65,34 @@ calcViet.onclick = function() {
     const [ paramA, paramB, paramC ] = _parameters; 
     const discriminant = (paramB * paramB) - ((paramA * paramC) * 4);
 
-    alert('Эта функциональность пока не реализована.');
+    let rootsSumAndMultiply = '';
+    if (paramA === 1) {
+        rootsSumAndMultiply += `<li>x<sub>1</sub> + x<sub>2</sub> = ${rounder(-paramB)}</li>`;        
+        rootsSumAndMultiply += `<li>x<sub>1</sub>x<sub>2</sub> = ${rounder(paramC)}</li>`;
+    } else {
+        rootsSumAndMultiply += `<li>x<sub>1</sub> + x<sub>2</sub> = ${rounder(-paramB / paramA)}</li>`;
+        rootsSumAndMultiply += `<li>x<sub>1</sub>x<sub>2</sub> = ${rounder(paramC / paramA)}</li>`;
+    }
+
+    let rootsAndDiscriminant = `<li>Дискриминант: ${discriminant}</li>`; 
+    if (discriminant < 0) rootsAndDiscriminant += `<li>У уравнения нет действительных корней.</li>` 
+    else if (discriminant === 0) rootsAndDiscriminant +=  `<li>x<sub>1</sub>, x<sub>2</sub> = ${rounder(Math.sqrt(paramC / paramA))}</li>`
+    else rootsAndDiscriminant += `<li>У уравнения два действительных корня. Найди их перебором.</li>` 
+
+    answer.innerHTML = `
+        <h4>Дискриминант, сумма и произведение корней</h4>
+        <ul>${rootsSumAndMultiply}</ul>
+        <h4>Корни и дискриминант</h4>
+        <ul>${rootsAndDiscriminant}</ul>
+    `
+    answer.innerHTML += `
+    <h4>Подсказки</h4>
+    <div class="block">
+        <ul>
+            <li>Формула дискриминанта: <i>b<sup>2</sup> - 4ac</i></li>
+            <li>Сумма корней: <i>-b / a</i></li>
+            <li>Произведение корней: <i>c / a</i></li>
+        </ul>
+    </div>
+    `
 }
